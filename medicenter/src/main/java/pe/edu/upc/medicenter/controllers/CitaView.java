f7cca556491895bbb64b6a895abd5a44c36bf448
Erdogp <54128481+Erdogp@users.noreply.github.com>
2020-09-24 20:06:21 -0500

    Entidades modificadas

---
+++ medicenter/src/main/java/pe/edu/upc/medicenter/controllers/CitaView.java
@@ -0,0 +1,5 @@
+package pe.edu.upc.medicenter.controllers;
+
+public class CitaView {
+
+}
---
+++ medicenter/src/main/java/pe/edu/upc/medicenter/models/entities/MedicamentosXClinica.java
@@ -0,0 +1,39 @@
+package pe.edu.upc.medicenter.models.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="medicamentosXclinicas")
+public class MedicamentosXClinica {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idMedClinica;
+	
+	@ManyToOne
+	@JoinColumn(name="medicamento_ID")
+	private Medicamento medicamento;
+	@ManyToOne
+	@JoinColumn(name="clinica_id")
+	private Clinica clinica;
+	
+	@OneToMany(mappedBy="medicamentos")
+	private List<Receta> recetas;
+
+	public MedicamentosXClinica() {
+		recetas = new ArrayList<>();
+	}
+	
+	
+	
+}

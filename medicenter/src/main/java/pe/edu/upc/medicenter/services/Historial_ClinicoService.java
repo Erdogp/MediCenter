63cdc439d139ee8b8220202ddcfce01b80f482e9
hiraoka <hiraoka@MSI>
2020-10-02 00:55:45 -0500

    Historial clinico Repository Impl

---
+++ medicenter/src/main/java/pe/edu/upc/medicenter/services/Historial_ClinicoService.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.medicenter.services;
+
+import java.util.Optional;
+
+import pe.edu.upc.medicenter.models.entities.Historial_Clinico;
+
+public interface Historial_ClinicoService extends CrudService<Historial_Clinico, Integer> {
+	
+}
